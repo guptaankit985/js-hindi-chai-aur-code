37a47af5a17f76b641ed192f8aad317532ce5c17
Rojin Chhetri <68652932+RojinxChhetri@users.noreply.github.com>
2024-09-14 14:54:01 +1000

    Fixed Undefined Return Value from Void Function.
    I have gave a solution why you were getting undefined output while logging the function.

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -33,6 +33,11 @@ JsUser.greeting = function(){
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
-
+// As we can see you was getting undefined output downside of Hello JS User, that's becuase the function jsUSer.greeting() doesn't return a value, so console.log() logs the default return of undefined.
+// Simple solution is remove console.log and just do Jsuser.greeting()
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+// Solution
+JsUser.greeting()
+JsUser.greetingTwo()
