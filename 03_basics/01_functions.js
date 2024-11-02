fcde024dad226cfd954162f274cf562e04be0c16
devanshi gupta <127221653+Devu0503@users.noreply.github.com>
2024-11-02 15:41:58 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,3 @@
-
 function sayMyName(){
     console.log("H");
     console.log("I");
@@ -9,41 +8,46 @@ function sayMyName(){
 }
 
 // sayMyName()
-
-// function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
-// }
+// Output: 
+// H
+// I
+// T
+// E
+// S
+// H
+// Note: sayMyName() simply prints each letter in "HITESH" on a new line.
 
 function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
     return number1 + number2
 }
 
 const result = addTwoNumbers(3, 5)
-
 // console.log("Result: ", result);
-
+// Output: "Result: 8"
+// Note: This is a basic function that adds two numbers. Important for interviews as it demonstrates return statements and function parameters.
 
 function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("PLease enter a username");
+        console.log("Please enter a username");
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+// Output: "hitesh just logged in"
 
+// console.log(loginUserMessage())
+// Output: "sam just logged in"
+// Note: This function showcases default parameters (username = "sam") and early returns if no username is provided. Important in interviews to show understanding of default values and return behavior.
 
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
+// Output: [500, 2000]
+// Note: Using the rest operator (`...num1`), this function collects additional arguments into an array. Valuable for understanding how to handle variable numbers of arguments.
 
 const user = {
     username: "hitesh",
@@ -55,10 +59,14 @@ function handleObject(anyobject){
 }
 
 // handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
+// Output: "Username is hitesh and price is undefined"
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+// Output: "Username is sam and price is 399"
+// Note: In the first call, there’s a typo in `prices`, which should be `price`. This highlights debugging skills when dealing with property names in objects.
 
 const myNewArray = [200, 400, 100, 600]
 
@@ -67,4 +75,6 @@ function returnSecondValue(getArray){
 }
 
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+// Output: 400
+// Note: This function retrieves the second element in an array. This is simple yet shows an understanding of array indexing.
+
