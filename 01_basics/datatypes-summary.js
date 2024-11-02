74d302db83662f2fd5c5f1f6c32aa54de2fd4923
devanshi gupta <127221653+Devu0503@users.noreply.github.com>
2024-11-02 15:03:36 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,37 +1,39 @@
-//  Primitive
+// Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;            // Number type
+const scoreValue = 100.3;    // Number type
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const isLoggedIn = false;     // Boolean type
+const outsideTemp = null;     // null type
+let userEmail;                // undefined type (since it is declared but not assigned)
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// Symbol is a unique and immutable data type used as identifiers for object properties
+const id = Symbol('123');               // Symbol type
+const anotherId = Symbol('123');        // Symbol type (different from 'id' despite having the same description)
 
-console.log(id === anotherId);
-
-// const bigNumber = 3456543576654356754n
-
+console.log(id === anotherId);          // false; Symbols are unique even if they have the same description
+// Note: Symbols are used to create unique property keys that won't collide with other keys.
 
 
-// Reference (Non primitive)
+// const bigNumber = 3456543576654356754n; // BigInt type (commented out, but would be a BigInt)
+
+// Reference (Non-primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["shaktiman", "naagraj", "doga"]; // Array type
 let myObj = {
-    name: "hitesh",
+    name: "hitesh",   // Object with properties
     age: 22,
-}
+};
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+const myFunction = function() {
+    console.log("Hello world"); // Function type
+};
 
-console.log(typeof anotherId);
+console.log(typeof anotherId); // "symbol"; returns the type of 'anotherId' variable
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// Note: 'typeof' is a unary operator that returns a string indicating the type of the unevaluated operand.
+// In this case, it will return "symbol" because 'anotherId' is a Symbol.
