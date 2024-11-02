6bd4599512551b87fdf322f9a5ed0a062fae6779
devanshi gupta <127221653+Devu0503@users.noreply.github.com>
2024-11-02 15:29:52 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,7 +5,8 @@ tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// tinderUser now contains:
+// { id: "123abc", name: "Sammy", isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -18,17 +19,18 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+// Output: "hitesh"
+// Accesses a deeply nested property using dot notation.
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
-
+// Merging obj1 and obj2 into obj3 using the spread operator
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
-
+// Output: { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// Note: Each object’s properties are added; if there were duplicate keys, the last object’s value would overwrite earlier ones.
 
 const users = [
     {
@@ -45,15 +47,25 @@ const users = [
     },
 ]
 
+// Accessing email of the second user
 users[1].email
-// console.log(tinderUser);
+// Output: "h@gmail.com"
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+// Output: ['id', 'name', 'isLoggedIn']
+// Object.keys() returns an array of property names in tinderUser.
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(Object.values(tinderUser));
+// Output: ['123abc', 'Sammy', false]
+// Object.values() returns an array of property values in tinderUser.
 
+console.log(Object.entries(tinderUser));
+// Output: [['id', '123abc'], ['name', 'Sammy'], ['isLoggedIn', false]]
+// Object.entries() returns an array of key-value pairs, each in an array format.
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// Output: true
+// hasOwnProperty() checks if 'isLoggedIn' exists directly on tinderUser, returning true.
 
 const course = {
     coursename: "js in hindi",
@@ -61,22 +73,27 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
-
+// Destructuring courseInstructor and renaming it to 'instructor'
 const {courseInstructor: instructor} = course
 
-// console.log(courseInstructor);
+// console.log(courseInstructor); 
+// Uncaught ReferenceError: courseInstructor is not defined
+// Explanation: Since we used destructuring with renaming, 'courseInstructor' doesn’t exist in the current scope; we only have 'instructor'.
+
 console.log(instructor);
+// Output: "hitesh"
+// instructor holds the value of course.courseInstructor due to destructuring with renaming.
 
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
+// Sample JSON structure
+{
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+}
 
+// This is an array of empty objects, often used as placeholders:
 [
     {},
     {},
     {}
 ]
-
