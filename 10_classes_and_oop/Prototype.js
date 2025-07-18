ebb8b5cfb46b129a831d5f8cba8d97e1462b8c0d
Aliahmad Maqsood <Aliahmadmaqsood57@gmail.com>
2025-07-18 11:53:41 +0500

    Update Prototype.js

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -1,67 +1,31 @@
-// let myName = "hitesh     "
-// let mychannel = "chai     "
+const user = {
+  userName: "Sunny",
+  loginCount: 9,
+  signedIn: true,
 
-// console.log(myName.trueLength);
+  getUserDetails: function () {
+    console.log("Got user details from database");
+    console.log(`Username: ${this.username}`);
+    console.log(this);
+  },
+};
 
+console.log(user.userName);
 
-let myHeros = ["thor", "spiderman"]
+function User(userName, loginCount, signedIn) {
+  this.userName = userName;
+  this.loginCount = loginCount;
+  this.signedIn = signedIn;
 
-
-let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
-
-    getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`);
-    }
+  return this; // not important //
 }
 
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
-}
+this.greeting = function () {
+  console.log(`Welcome ${this.username}`);
+};
 
-Array.prototype.heyHitesh = function(){
-    console.log(`Hitesh says hello`);
-}
+const userOne = new User("Sunny", 9, true);
+const userTwo = new User("Sam", 33, true);
+console.log(userOne);
 
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
 
-// inheritance
-
-const User = {
-    name: "chai",
-    email: "chai@google.com"
-}
-
-const Teacher = {
-    makeVideo: true
-}
-
-const TeachingSupport = {
-    isAvailable: false
-}
-
-const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport
-}
-
-Teacher.__proto__ = User
-
-// modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
-
-let anotherUsername = "ChaiAurCode     "
-
-String.prototype.trueLength = function(){
-    console.log(`${this}`);
-    console.log(`True length is: ${this.trim().length}`);
-}
-
-anotherUsername.trueLength()
-"hitesh".trueLength()
-"iceTea".trueLength()
